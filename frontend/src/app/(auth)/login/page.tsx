bf94905dfeaa062613010e2d2627c7a937f8638e
Sergionx <sergio180203@gmail.com>
2024-09-29 19:54:53 -0400

    feat: Add login and sign-up forms. Only UI

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LoginForm } from "./LoginForm";
+
+export default function page() {
+  return (
+    <div className="flex h-svh items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SignUpForm } from "./SignUpForm";
+
+export default function page() {
+  return (
+    <div className="flex h-svh items-center justify-center">
+      <SignUpForm />
+    </div>
+  );
+}
