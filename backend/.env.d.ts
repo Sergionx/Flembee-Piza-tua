027d4f6bdea5ca5d18e491cd0f9d8e4b51928571
Sergionx <sergio180203@gmail.com>
2024-10-02 13:15:47 -0400

    chore: Add import paths and typing on .env variables

--- backend/.env.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    JWT_ACCESS_SECRET: string;
-  }
-}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import 'tsconfig-paths/register';
 
-import pizzaRoutes from './routes/pizzas.routes'
-import userRoutes from './routes/users.routes'
+import pizzaRoutes from '@/api/pizzas/pizzas.routes'
+import userRoutes from '@/api/users/users.routes'
+import authRoutes from '@/api/auth/auth.routes'
 
 const PORT = 3001	
 
@@ -11,6 +13,7 @@ app.use(express.json())
 
 app.use('/api/pizzas', pizzaRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/auth', authRoutes)
 
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`)
