10ebdd9a76608e87453b82b33bf661988d23027e
Sergionx <sergio180203@gmail.com>
2024-10-04 20:58:17 -0400

    feat: enhance user columns with role enum handling and date formatting; add updatedAt column and dialog state management

--- frontend/src/app/(protected)/users/user-columns.tsx
+++ frontend/src/app/(protected)/users/user-columns.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { MoreHorizontal, Pencil, Trash } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -11,13 +12,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-
-import type { User } from "@/lib/interfaces/User";
-import type { ColumnDef } from "@tanstack/react-table";
-import EditUserDialog from "./components/edit-user-dialog/edit-user-dialog";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-import { useState } from "react";
+
+import EditUserDialog from "./components/edit-user-dialog/edit-user-dialog";
 import DeleteUserDialog from "./components/delete-user-dialog";
+
+import { Role, type User } from "@/lib/interfaces/User";
+import type { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<User>[] = [
   {
@@ -33,33 +34,49 @@ export const columns: ColumnDef<User>[] = [
     accessorKey: "email",
   },
   {
-    header: "Rol",
+    accessorKey: "role",
+    header: () => <div className="text-center">Rol</div>,
     cell: ({ row }) => {
       const { role } = row.original;
-      const roleStr = role === "admin" ? "Administrador" : "Usuario";
+      const roleStr = role === Role.ADMIN ? "Administrador" : "Usuario";
 
       return (
-        <span
-          className={cn("px-2 rounded-md py-1", {
-            "bg-red-500 text-white": role === "admin",
-            "bg-blue-500 text-white": role === "user",
-          })}
-        >
-          {roleStr}
-        </span>
+        <div className="flex justify-center">
+          <span
+            className={cn("px-2 rounded-md py-1", {
+              "bg-red-500 text-white": role === Role.ADMIN,
+              "bg-blue-500 text-white": role === Role.USER,
+            })}
+          >
+            {roleStr}
+          </span>
+        </div>
       );
     },
   },
   {
     accessorKey: "createdAt",
-    header: () => <div className="text-center">Fecha de creación</div>,
+    header: () => <div className="text-center">Fecha de Creación</div>,
 
     cell: ({ row }) => {
       const { createdAt } = row.original;
+
+      const date = new Date(createdAt);
       return (
-        <div className="text-center">
-          {createdAt.toLocaleDateString("en-US")}
-        </div>
+        <div className="text-center">{date.toLocaleDateString("es-ES")}</div>
+      );
+    },
+  },
+  {
+    accessorKey: "updatedAt",
+    header: () => <div className="text-center">Fecha de Modificación</div>,
+
+    cell: ({ row }) => {
+      const { updatedAt } = row.original;
+
+      const date = new Date(updatedAt);
+      return (
+        <div className="text-center">{date.toLocaleDateString("es-ES")}</div>
       );
     },
   },
@@ -73,10 +90,18 @@ export const columns: ColumnDef<User>[] = [
         deleteDialog = "dialog2",
       }
 
-      const [currentDialog, setCurrentDialog] = useState<Dialogs | null>();
+      const [currentDialog, setCurrentDialog] = useState<Dialogs | null>(null);
+      const isOpen = currentDialog !== null;
 
       return (
-        <Dialog>
+        <Dialog
+          open={isOpen}
+          onOpenChange={(isOpen) => {
+            if (!isOpen) {
+              setCurrentDialog(null);
+            }
+          }}
+        >
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="h-8 w-8 p-0">
@@ -91,6 +116,7 @@ export const columns: ColumnDef<User>[] = [
               <DialogTrigger
                 asChild
                 onClick={() => {
+                  console.log("Editando usuario", user.email);
                   setCurrentDialog(Dialogs.editDialog);
                 }}
               >
@@ -114,7 +140,10 @@ export const columns: ColumnDef<User>[] = [
             </DropdownMenuContent>
 
             {currentDialog === Dialogs.editDialog ? (
-              <EditUserDialog user={user} />
+              <EditUserDialog
+                user={user}
+                close={() => setCurrentDialog(null)}
+              />
             ) : (
               <DeleteUserDialog user={user} />
             )}
