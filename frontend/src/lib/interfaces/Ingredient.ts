532e4e404dd11cd483ecca8811273c5ce58f8d1b
Sergionx <sergio180203@gmail.com>
2024-10-06 12:56:26 -0400

    feat: extend Ingredient interface to include unit and change date types

--- frontend/src/lib/interfaces/Ingredient.ts
+++ frontend/src/lib/interfaces/Ingredient.ts
@@ -1,10 +1,18 @@
+export enum Unit {
+  KG = "KG",
+  G = "G",
+  L = "L",
+  ML = "ML"
+}
+
 export interface Ingredient {
   id: string
   name: string
   price: number
   stock: number
-  createdAt: Date
-  updatedAt: Date
+  unit: Unit
+  createdAt: string
+  updatedAt: string
 }
 
 export interface PizzaIngredient {
