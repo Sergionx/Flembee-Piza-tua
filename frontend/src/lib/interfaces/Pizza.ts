fb1b2808a725b48481e1c4ad66fa66c818751a02
Sergionx <sergio180203@gmail.com>
2024-10-09 10:28:58 -0400

    feat: extend Pizza interface to include price and ingredients details

--- frontend/src/lib/interfaces/Pizza.ts
+++ frontend/src/lib/interfaces/Pizza.ts
@@ -1,5 +1,19 @@
+import { Unit } from "./Ingredient";
+
 export interface Pizza {
-  id: string
-  name: string
-  // price: number
+  id: string;
+  name: string;
+}
+
+export interface PizzaWithIngredients extends Pizza {
+  price: number;
+  ingredients: {
+    id: string;
+    name: string;
+    quantity: number;
+    stock: number;
+    price: number;
+    unitPrice: Unit;
+    unitQuantity: Unit;
+  }[];
 }
